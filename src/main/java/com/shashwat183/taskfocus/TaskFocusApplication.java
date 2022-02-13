c46683a4c3ab2e2c1c974a003fdd6d5311ed8d41
shashwat pragya <shashwat183@gmail.com>
2022-02-13 18:13:52 +0000

    Initial Commit

---
+++ src/main/java/com/shashwat183/taskfocus/TaskFocusApplication.java
@@ -0,0 +1,13 @@
+package com.shashwat183.taskfocus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskFocusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskFocusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shashwat183/taskfocus/TaskFocusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shashwat183.taskfocus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskFocusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
